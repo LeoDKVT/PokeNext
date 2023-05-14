799b9aa3f55016d8b62723ff8f14eee466e709b8
José Leonardo <leodkvt@gmail.com>
2023-05-14 10:19:52 -0300

    Inicio do projeto em nextjs

---
+++ my-app/src/components/Layoutt.js
@@ -0,0 +1,18 @@
+import Head from "next/head";
+import Navbar from "./Navbar";
+import Footer from "./Footer";
+
+
+export default function Layout({children}) {
+    return (
+        <>
+            <Head>
+                <link rel="shortcut icon" href="/images/favicon.ico"/>
+                <title>PokeNext</title>
+            </Head>
+            <Navbar />
+            <main></main>
+            <Footer />
+        </>
+    )
+}
